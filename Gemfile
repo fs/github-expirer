f39fe462fb38f0f2ad677ce4fb43ccf8e7c4b732
Timur Batyrshin <timur.batyrshin@flathost.ru>
2012-09-20 20:41:24 +0400

    major refactoring (thanks to @timurvafin for help)

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source :rubygems
 
+gem 'activesupport'
 gem 'github_api'
 gem 'chronic'
--- expirer.rb
+++ expirer.rb
@@ -1,45 +1,62 @@
 #!/usr/bin/env ruby
 
+require 'rubygems'
+require 'bundler/setup'
+
+require 'active_support/all'
 require 'github_api'
 require 'time'
 require 'chronic'
 require 'yaml'
 
-class Expirer
-  def initialize(config)
-    @config ||= {}.merge( config )
-
-    @github = Github.new basic_auth: "#{@config["username"]}:#{@config["password"]}"
-
-    @repos = @github.repos.all( org: @config["org"] )
-
-    @expire = Chronic.parse( @config["expire"] || '1 year ago' )
-
-    @private_only = @config["private"]
-  end
-
-  def run
-    @repos.reject { |repo| !check_repo(repo) }.map { |repo| repo_data(repo) }
-  end
-
-private
-
-  def private_string(is_private)
-    is_private ? "private" : "public"
-  end
-
-  def check_repo(repo)
-    Time.parse( repo.pushed_at ) < @expire && (!@private_only || repo.private)
-  end
-
-  def repo_data(repo)
-    [ repo.pushed_at, repo.git_url, private_string(repo.private) ]
+class Hashie::Mash
+  def to_s
+    [pushed_at, html_url, private ? 'private' : 'public'].join(' ')
   end
 end
 
-config = YAML.load( File.read('config.yml') )
+class Expirer
+  attr_accessor :config
 
-expirer = Expirer.new( config )
-repos = expirer.run
+  def initialize(config)
+    @config = {
+      :expire_date  => '1 year ago',
+      :private_only => false,
+    }.merge(config.symbolize_keys)
+  end
 
-puts repos.sort.map { |x| x.join(" ")}
+  def expired_repositories
+    repositories.select do |repository|
+      private?(repository) && expired?(repository)
+    end.sort_by(&:pushed_at)
+  end
+
+  private
+
+  def expire_date
+    @expire_date ||= Chronic.parse(@config[:expire_date])
+  end
+
+  def github
+    @github ||= Github.new(:basic_auth => "#{@config[:username]}:#{@config[:password]}")
+  end
+
+  def repositories
+    @repositories ||= github.repos.all(:org => @config[:organization])
+  end
+
+  def private?(repository)
+    return true unless config[:private_only]
+    repository.private
+  end
+
+  def expired?(repository)
+    Time.parse(repository.pushed_at) < expire_date
+  end
+end
+
+config = YAML.load(File.read('config.yml'))
+
+Expirer.new(config).expired_repositories.each do |repository|
+  puts repository
+end
